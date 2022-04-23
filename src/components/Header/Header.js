ef41826c41eaed09538abfb5dded09f742ee1433
Jonathan Buttigieg <jonathanbuttigieg1@gmail.com>
2022-04-23 15:19:25 +0200

    query methods testing react

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,23 @@ import { Typography } from '@mui/material';
 const Header = ({ title }) => {
   return (
     <>
-      <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
+      <Typography
+        data-testid="header-1"
+        component="h1"
+        variant="h2"
+        align="center"
+        color="text.primary"
+        gutterBottom>
         {title}
+      </Typography>
+      <Typography
+        title="Header"
+        component="h3"
+        variant="h3"
+        align="center"
+        color="text.primary"
+        gutterBottom>
+        ZzzZ
       </Typography>
     </>
   );
