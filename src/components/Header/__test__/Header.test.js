2629911bd19e210b220020a6544b2f98e7b2c3e1
Jonathan Buttigieg <jonathanbuttigieg1@gmail.com>
2022-04-23 16:00:30 +0200

    describe blocks

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,54 +1,50 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it('should render same text passed into title prop', () => {
-  render(<Header title="test" />);
-  const headingELement = screen.getByText(/test/i);
-  expect(headingELement).toBeInTheDocument();
-});
+describe('Header', () => {
+  it('should render same text passed into title prop', () => {
+    render(<Header title="test" />);
+    const headingELement = screen.getByText(/test/i);
+    expect(headingELement).toBeInTheDocument();
+  });
 
-// it('should render same text passed into title prop', () => {
-//   render(<Header title="test" />);
-//   const headingELement = screen.getByRole('heading');
-//   expect(headingELement).toBeInTheDocument();
-// });
+  it('should render same text passed into title prop', () => {
+    render(<Header title="test" />);
+    const headingELement = screen.getByRole('heading', { name: 'test' });
+    expect(headingELement).toBeInTheDocument();
+  });
 
-it('should render same text passed into title prop', () => {
-  render(<Header title="test" />);
-  const headingELement = screen.getByRole('heading', { name: 'test' });
-  expect(headingELement).toBeInTheDocument();
-});
+  it('should render same text passed into title prop', () => {
+    render(<Header title="test" />);
+    const headingELement = screen.getByTitle('Header');
+    expect(headingELement).toBeInTheDocument();
+  });
 
-it('should render same text passed into title prop', () => {
-  render(<Header title="test" />);
-  const headingELement = screen.getByTitle('Header');
-  expect(headingELement).toBeInTheDocument();
-});
+  it('should render same text passed into title prop', () => {
+    render(<Header title="test" />);
+    const headingELement = screen.getByTestId('header-1');
+    expect(headingELement).toBeInTheDocument();
+  });
 
-it('should render same text passed into title prop', () => {
-  render(<Header title="test" />);
-  const headingELement = screen.getByTestId('header-1');
-  expect(headingELement).toBeInTheDocument();
-});
+  // FIND BY
 
-// FIND BY
+  it('should render same text passed into title prop', async () => {
+    render(<Header title="test" />);
+    const headingELement = await screen.findByText(/test/i);
+    expect(headingELement).toBeInTheDocument();
+  });
 
-it('should render same text passed into title prop', async () => {
-  render(<Header title="test" />);
-  const headingELement = await screen.findByText(/test/i);
-  expect(headingELement).toBeInTheDocument();
-});
+  // QueryBY
 
-// QueryBY
+  it('should render same text passed into title prop', async () => {
+    render(<Header title="test" />);
+    const headingELement = screen.queryByText(/dogs/i);
+    expect(headingELement).not.toBeInTheDocument();
+  });
 
-it('should render same text passed into title prop', async () => {
-  render(<Header title="test" />);
-  const headingELement = screen.queryByText(/dogs/i);
-  expect(headingELement).not.toBeInTheDocument();
-});
-
-it('should render same text passed into title prop', async () => {
-  render(<Header title="test" />);
-  const headingELement = screen.getAllByRole('heading');
-  expect(headingELement.length).toBe(2);
+  it('should render same text passed into title prop', async () => {
+    render(<Header title="test" />);
+    const headingELement = screen.getAllByRole('heading');
+    expect(headingELement.length).toBe(2);
+  });
 });
--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -9,51 +9,52 @@ const MockTodoFooter = incompleteTasksCount => {
     </BrowserRouter>
   );
 };
+describe('TodoFooter', () => {
+  it('should render the correct amount of incomplete tasks', () => {
+    render(MockTodoFooter(2));
+    const divElement = screen.getByText(/2 tasks left/i);
+    expect(divElement).toBeInTheDocument();
+  });
 
-it('should render the correct amount of incomplete tasks', () => {
-  render(MockTodoFooter(2));
-  const divElement = screen.getByText(/2 tasks left/i);
-  expect(divElement).toBeInTheDocument();
-});
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement).toBeInTheDocument();
+  });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement).toBeInTheDocument();
-});
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement).toBeTruthy();
+  });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement).toBeTruthy();
-});
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement).toBeVisible();
+  });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement).toBeVisible();
-});
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement).toContainHTML('div');
+  });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement).toContainHTML('div');
-});
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement).toHaveTextContent('1 task left');
+  });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement).toHaveTextContent('1 task left');
-});
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement).not.toBeFalsy();
+  });
 
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement).not.toBeFalsy();
-});
-
-it('should render "task" when the number of incomplete tasks is one', () => {
-  render(MockTodoFooter(1));
-  const divElement = screen.getByText(/1 task left/i);
-  expect(divElement.textContent).toBe('1 task left');
+  it('should render "task" when the number of incomplete tasks is one', () => {
+    render(MockTodoFooter(1));
+    const divElement = screen.getByText(/1 task left/i);
+    expect(divElement.textContent).toBe('1 task left');
+  });
 });
